f0f4d8bf1af4b57a7b6006eceb9d00ee3166bb21
nedoratic <nedoratic@gmail.com>
2024-01-16 23:13:38 +0100

    basic calculator functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,72 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [answer, setAnswer] = useState('');
+	const [expression, setExpression] = useState('');
+	const et = expression.trim();
+
+	const isOperator = (symbol: string) => {
+		return /[*/+-]/.test(symbol);
+	};
+
 	const buttonPress = (symbol: string) => {
-		console.log(symbol);
+		if (symbol === 'clear') {
+			setAnswer('');
+			setExpression('0');
+		} else if (symbol === 'negative') {
+			if (answer === '') return;
+			setAnswer(answer.toString().charAt(0) === '-' ? answer.slice(1) : '-' + answer);
+		} else if (symbol === 'percentage') {
+			if (answer === '') return;
+			setAnswer((parseFloat(answer) / 100).toString());
+		} else if (isOperator(symbol)) {
+			setExpression(et + ' ' + symbol + ' ');
+		} else if (symbol === '=') {
+			calculate();
+		} else if (symbol === '0') {
+			if (expression.charAt(0) !== '0') {
+				setExpression(expression + symbol);
+			}
+		} else if (symbol === '.') {
+			const lastNumber = expression.split(/[-+/*]/g).pop();
+			if (!lastNumber) return;
+			console.log('lastNumber :>> ', lastNumber);
+			if (lastNumber?.includes('.')) return;
+			setExpression(expression + symbol);
+		} else {
+			if (expression.charAt(0) === '0') {
+				setExpression(expression.slice(1) + symbol);
+			} else {
+				setExpression(expression + symbol);
+			}
+		}
+	};
+
+	const calculate = () => {
+		if (isOperator(et.charAt(et.length - 1))) return;
+		const parts = et.split(' ');
+		const newParts = [];
+		for (let i = parts.length - 1; i >= 0; i--) {
+			if (['*', '/', '+'].includes(parts[i]) && isOperator(parts[i - 1])) {
+				newParts.unshift(parts[i]);
+				let j = 0;
+				let k = i - 1;
+				while (isOperator(parts[k])) {
+					k--;
+					j++;
+				}
+				i -= j;
+			} else {
+				newParts.unshift(parts[i]);
+			}
+		}
+		const newExpression = newParts.join(' ');
+		if (isOperator(newExpression.charAt(0))) {
+			setAnswer(eval(answer + newExpression) as string);
+		} else {
+			setAnswer(eval(newExpression) as string);
+		}
+		setExpression('');
 	};
 
 	return (
@@ -12,8 +76,8 @@ function App() {
 				<h1>Calculator</h1>
 				<div id="calculator">
 					<div id="display">
-						<div id="answer">0</div>
-						<div id="expression">0</div>
+						<div id="answer">{answer}</div>
+						<div id="expression">{expression}</div>
 					</div>
 					<button className="light-gray" id="clear" onClick={() => buttonPress('clear')}>
 						AC
